15e7a634c118a211074c1f8360a0a587acec6ab1
Shivam8604 <145273089+Shivam8604@users.noreply.github.com>
2024-01-01 23:05:06 +0530

    edit profile section

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let input = document.querySelector(".search");
-console.log(input)
-
-input.addEventListener('keydown', () =>{
-    if(input.value){
-        document.getElementById("close").style.display = "block";
-    }else{
-        document.getElementById("close").style.display = "none"
+function close() {
+  let input = document.querySelector(".search");
+  input.addEventListener("keydown", () => {
+    if (input.value) {
+      document.getElementById("close").style.display = "block";
+    } else {
+      document.getElementById("close").style.display = "none";
     }
-});
+  });
+};
+close();
+
+//  user profile
+
+let userAccount = document.querySelector(".profileIcon");
+
+userAccount.addEventListener('mouseenter',()=>{
+    document.querySelector('#user').style.display = "block";
+})
