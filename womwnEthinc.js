2d9da4da3c996e17b3547710bc70227919e1ff5a
Shivam8604 <145273089+Shivam8604@users.noreply.github.com>
2024-01-03 15:37:14 +0530

    add womenEthnic

---
+++ womwnEthinc.js
@@ -0,0 +1,26 @@
+const WomenEthnic = [
+    {
+        heading : "All Women Ethnic",
+        data : ["View All"]
+    },
+    {
+        heading : "Sarees",
+        data : ["All Sarees","Silk Sarees","cotton silk Sarees","Cotton Sarees","Georgette sarees"]
+    },
+    {
+        heading : "",
+        data : []
+    },
+    {
+        heading : "",
+        data : []
+    },
+    {
+        heading : "",
+        data : []
+    },
+    {
+        heading : "",
+        data : []
+    }
+]
